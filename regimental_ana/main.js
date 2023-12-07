2bd0282bf09b95ba4180bf6b722982d9c36073bd
Ana Cristie <124894026+PastelDeQueijo09090@users.noreply.github.com>
2023-12-07 10:20:07 -0300

    Add files via upload

---
+++ regimental_ana/main.js
@@ -0,0 +1,19 @@
+
+
+alert("testanso javascript externo")
+
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+ let lista = document.querySelectorAll(".tecla");
+   let contador = 0;
+   while (contador <lista.length){
+    const efeito = lista[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    lista [contador].onclick = function(){
+        tocaSom (idAudio);
+    }
+    contador++;
+}
+    
